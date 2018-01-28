5a84a3b83f674f93856e892565e9a94fb5b95acd
Harvey Bal <harvey_bal@hotmail.co.uk>
2018-01-28 12:47:23 +0000

    Added args for email and phone

--- generate.bash
+++ generate.bash
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+while getopts ":p:e:" arg; do
+	case "${arg}" in
+		p)
+		  PHONE=${OPTARG}
+		  ;;
+		e)
+		  EMAIL=${OPTARG}
+		  ;;
+	esac
+done
+
 #Convert YAML to JSON
 python ./src/python/yml2json.py -i ./resume/resume.yml -o ./resume/resume.json
 
 #Run Docker Command 
 docker run -e 'OUTPUT_TEMPLATE=positive' \
-		-e 'PHONE=0123456789' \
-		-e 'EMAIL=email@email.com' \
-		-e 'ADDRESS=myAddress' \
-		-e 'CITY=City' \
-		-e 'CODE=Code' \
-		-e 'DISPLAY=unix$DISPLAY' \
-		-v $(pwd)/resume/:/usr/share/nginx/html/ \
-		-v /tmp/.X11-unix:/tmp/.X11-unix \
-		nouchka/hackmyresume
+               -e "PHONE=${PHONE}" \
+               -e "EMAIL=${EMAIL}" \
+               -e 'ADDRESS=myAddress' \
+               -e 'CITY=City' \
+               -e 'CODE=Code' \
+               -e 'DISPLAY=unix$DISPLAY' \
+               -v $(pwd)/resume/:/usr/share/nginx/html/ \
+               -v /tmp/.X11-unix:/tmp/.X11-unix \
+               nouchka/hackmyresume
