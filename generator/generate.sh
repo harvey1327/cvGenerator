3f56bf0c54a72c273b9e125d1627692a8e34403d
Harvey Bal <harvey_bal@hotmail.co.uk>
2022-01-24 12:29:58 +0000

    updated

--- generator/generate.sh
+++ generator/generate.sh
@@ -4,8 +4,6 @@ RESUME_FILE=/resume/resume.json
 
 sed -i 's/###PHONE###/'$PHONE'/' $RESUME_FILE
 sed -i 's/###EMAIL###/'$EMAIL'/' $RESUME_FILE
-sed -i "s/###CITY###/$CITY/" $RESUME_FILE
-sed -i "s/###REGION###/$REGION/" $RESUME_FILE
 
 npm install -g jsonresume-theme-${THEME}
 
