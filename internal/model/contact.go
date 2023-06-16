e6408e26818f14a8fae6647a19192066ff6a21be
Harvey Bal <harvey_bal@hotmail.co.uk>
2023-06-16 15:45:56 +0100

    added certs field to model

--- internal/model/contact.go
+++ internal/model/contact.go
@@ -3,23 +3,4 @@ package model
 type contact struct {
 	Phone string
 	Email string
-	// EmailDisplay string
 }
-
-// func (c *contact) UnmarshalYAML(unmarshal func(interface{}) error) error {
-// 	type raw contact
-// 	value := (*raw)(c)
-// 	if err := unmarshal(value); err != nil {
-// 		return err
-// 	}
-// 	if strings.Contains(value.Email, "_") {
-// 		c.EmailDisplay = strings.ReplaceAll(value.Email, "_", "\\_")
-// 	} else {
-// 		c.EmailDisplay = value.Email
-// 	}
-// 	return nil
-// }
-
-// func (c *contact) ApplyEmail() {
-
-// }
--- internal/model/pageData.go
+++ internal/model/pageData.go
@@ -1,11 +1,12 @@
 package model
 
 type PageData struct {
-	Meta       Meta
-	Info       info
-	Contact    contact
-	Social     social
-	Employment employment
-	Education  education
-	Skills     skills
+	Meta         Meta
+	Info         info
+	Contact      contact
+	Social       social
+	Employment   employment
+	Education    education
+	Skills       skills
+	Certificates certificate
 }
